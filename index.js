b5978a55d21be10db44a790d92a3d07dab3185de
Mia Ruiz (Desktop) <miarg49@gmail.com>
2021-10-09 19:46:56 -0300

    sincro desktop

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const log = (p1) => console.log(p1);
 
 // http://rain.thecomicseries.com/comics/510    objetive site - id = comicimage
 
-const cheerio = require('cheerio');
-const request = require('request');
-const sources = require('./sources');
-const fs = require('fs');
+const cheerio = require('cheerio'),
+	request = require('request'),
+	shell = require('shelljs'),
+	fs = require('fs');
 
-const scraping = async (url, id, from, to) => {
+request.defaults({ encoding: null });
+
+const scraping = async (api, url, id, path, title, from, to) => {
 	let $ = null;
 
 	const headers = {
@@ -18,37 +20,77 @@ const scraping = async (url, id, from, to) => {
 	};
 
 	try {
-		do {
-			try {
-				const options = {
-					uri: `${url}/${from}`,
-					baseUrl: api,
-					method: 'GET',
-					headers: headers
-				};
-				await request.get(options, (err, response, html) => {
-					$ = cheerio.load(html);
+		shell.mkdir('-p', `${path}/${title}`);
+		try {
+			const options = {
+				uri: `/${url}/${from}`,
+				baseUrl: api,
+				method: 'GET',
+				headers: headers
+			};
+			await request.get(options, async (err, response, html) => {
+				$ = cheerio.load(html);
 
-					const image = $('img#comicimage');
-					log(image);
-					debugger;
-					// writeFile function with filename, content and callback function
-					// fs.writeFile('newfile.txt', 'Learn Node FS module', function (err) {
-					// 	if (err) throw err;
-					// 	console.log('File is created successfully.');
-					// });
-
-					log(results);
-				});
-			} catch (e) {
-				log(e);
-			}
-			from++;
-		} while (from < to);
+				const nodes = $(`img#${id}`);
+				if (nodes.length > 0) {
+					const node = nodes[0] ? nodes[0] : null;
+					if (node && node.name === 'img' && node.attribs.src !== 0) {
+						// log(node.attribs.src);
+						const partsBaseApi = String(node.attribs.src).split('/');
+						const fileExtension = partsBaseApi[partsBaseApi.length - 1].split('.')[1];
+						let newApi = `${partsBaseApi[0]}//${partsBaseApi[2]}`,
+							newUri = ``;
+						for (let index = 3; index < partsBaseApi.length; index++) {
+							newUri += `/${partsBaseApi[index]}`;
+						}
+						const newHeaders = {
+							accept: `text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/${fileExtension},*/*;q=0.8,application/signed-exchange;v=b3;q=0.9`,
+							'accept-language': 'en-US,en;q=0.9,es;q=0.8',
+							'cache-control': 'max-age=0',
+							'sec-ch-ua': '" Not;A Brand";v="99", "Microsoft Edge";v="91", "Chromium";v="91"',
+							'Content-Type': `image/${fileExtension}`
+						};
+						const newOptions = {
+							uri: newUri,
+							baseUrl: newApi,
+							method: 'GET',
+							headers: newHeaders,
+							encoding: 'binary'
+						};
+						await request.get(newOptions, (err, response, img) => {
+							// log(img);
+							// writeFile function with filename, content and callback function
+							fs.writeFile(
+								`../downloads/${title}/page${from}.${fileExtension}`,
+								img,
+								'binary',
+								function (err) {
+									if (err) throw err;
+									if (from < to) {
+										from++;
+										scraping(
+											'http://rain.thecomicseries.com',
+											'comics',
+											'comicimage',
+											'../downloads/',
+											'RAIN',
+											from,
+											to
+										);
+									}
+								}
+							);
+						});
+					}
+				}
+			});
+		} catch (e) {
+			// log(e);
+		}
 	} catch (e) {
-		log(e);
+		// log(e);
 	}
 };
 
 log(`it begin's...`);
-scraping('http://rain.thecomicseries.com/comics', 'comicimage', 1, 1);
+scraping('http://rain.thecomicseries.com', 'comics', 'comicimage', '../downloads/', 'RAIN', 51, 1405);
